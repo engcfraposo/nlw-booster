201659b3aae38dac8204bd234bdab2269d3c5ea9
engcfraposo <engcfraposo@gmail.com>
2020-06-02 02:36:11 -0300

    update 04/06/2020
    Desenvolvimento do front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import Routes from './routes'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Word</h1>
-      </header>
-    </div>
+    <>
+        <Routes />
+    </>
   );
 }
 
